9af6b6e75fe81cce46ebc09d8a1b455c6e16e583
Haqimi Imran <haqimiah05@gmail.com>
2026-01-14 08:47:11 +0800

    early commit, added task model and controller

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    
+});
+
+require __DIR__.'/settings.php';
