039f5dada345b30add491c77baa0c773af1fee55
Haqimi Imran <haqimiah05@gmail.com>
2026-01-14 21:24:15 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
+    // tasks route
+    Route::resource('tasks', TaskController::class);
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-    
+    Route::post('tasks/{task}/toggle', [TaskController::class, 'toggleComplete'])->name('tasks.toggle');
+    Route::post('tasks/bulk-delete', [TaskController::class, 'bulkDelete'])->name('tasks.bulk-delete');
+
 });
 
 require __DIR__.'/settings.php';
